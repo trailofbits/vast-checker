0f78b5aeb4fc942e7b675ce6b9af3483b796cf43
Marek Surovič <marek.surovic@trailofbits.com>
2023-04-17 18:22:41 +0200

    sequoia: adds tests extracted from linux kernel

---
+++ sequoia/extract.c
@@ -0,0 +1,289 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/** 
+ * include/linux/compiler_types.h
+ */
+
+# define __force
+
+/** 
+ * include/linux/compiler_attributes.h
+ */
+
+#define __must_check                    __attribute__((__warn_unused_result__))
+
+/** 
+ * tools/include/linux/err.h
+ */
+
+static inline bool __must_check IS_ERR(__force const void *ptr)
+{
+	return true;
+}
+
+/** 
+ * tools/bootconfig/include/linux/bootconfig.h
+ */
+
+#define WARN_ON(cond)	\
+	((cond) ? printf("Internal warning(%s:%d, %s): %s\n",	\
+			__FILE__, __LINE__, __func__, #cond) : 0)
+
+#define unlikely(cond)	(cond)
+
+/** 
+ * include/linux/compiler_types.h
+ */
+
+# define __designated_init
+# define __randomize_layout __designated_init
+
+/** 
+ * include/linux/dcache.h
+ */
+
+struct dentry;
+
+struct dentry_operations {
+	char *(*d_dname)(struct dentry *, char *, int);
+};
+
+struct dentry {
+	struct dentry *d_parent;	/* parent directory */
+	const struct dentry_operations *d_op;
+} __randomize_layout;
+
+#define IS_ROOT(x) ((x) == (x)->d_parent)
+
+static inline int d_unlinked(const struct dentry *dentry)
+{
+	return 0;
+}
+
+/** 
+ * include/linux/mount.h
+ */
+
+struct vfsmount {
+	struct dentry *mnt_root;	/* root of the mounted tree */
+} __randomize_layout;
+
+/** 
+ * include/linux/path.h
+ */
+
+struct path {
+	struct vfsmount *mnt;
+	struct dentry *dentry;
+} __randomize_layout;
+
+/** 
+ * include/linux/fs_struct.h
+ */
+
+struct fs_struct;
+
+struct task_struct {
+	struct fs_struct *fs;
+};
+
+/** 
+ * include/linux/rcupdate.h
+ */
+
+static __always_inline void rcu_read_lock(void)
+{
+}
+
+static inline void rcu_read_unlock(void)
+{
+}
+
+/** 
+ * ???
+ */
+
+struct task_struct *current;
+
+/** 
+ * fs/d_path.c
+ */
+
+struct prepend_buffer {
+	char *buf;
+	int len;
+};
+#define DECLARE_BUFFER(__name, __buf, __len) \
+	struct prepend_buffer __name = {.buf = __buf + __len, .len = __len}
+
+static void get_fs_root_rcu(struct fs_struct *fs, struct path *root)
+{
+}
+
+static bool prepend(struct prepend_buffer *p, const char *str, int namelen)
+{
+	return true;
+}
+
+static bool prepend_char(struct prepend_buffer *p, unsigned char c)
+{
+	return true;
+}
+/**
+ * prepend_path - Prepend path string to a buffer
+ * @path: the dentry/vfsmount to report
+ * @root: root vfsmnt/dentry
+ * @p: prepend buffer which contains buffer pointer and allocated length
+ *
+ * The function will first try to write out the pathname without taking any
+ * lock other than the RCU read lock to make sure that dentries won't go away.
+ * It only checks the sequence number of the global rename_lock as any change
+ * in the dentry's d_seq will be preceded by changes in the rename_lock
+ * sequence number. If the sequence number had been changed, it will restart
+ * the whole pathname back-tracing sequence again by taking the rename_lock.
+ * In this case, there is no need to take the RCU read lock as the recursive
+ * parent pointer references will keep the dentry chain alive as long as no
+ * rename operation is performed.
+ */
+static int prepend_path(const struct path *path,
+			const struct path *root,
+			struct prepend_buffer *p)
+{
+	return 0;
+}
+
+static char *extract_string(struct prepend_buffer *p)
+{
+	return NULL;
+}
+
+/**
+ * d_path - return the path of a dentry
+ * @path: path to report
+ * @buf: buffer to return value in
+ * @buflen: buffer length
+ *
+ * Convert a dentry into an ASCII path name. If the entry has been deleted
+ * the string " (deleted)" is appended. Note that this is ambiguous.
+ *
+ * Returns a pointer into the buffer or an error code if the path was
+ * too long. Note: Callers should use the returned pointer, not the passed
+ * in buffer, to use the name! The implementation often starts at an offset
+ * into the buffer, and may leave 0 bytes at the start.
+ *
+ * "buflen" should be positive.
+ */
+char *d_path(const struct path *path, char *buf, int buflen)
+{
+	DECLARE_BUFFER(b, buf, buflen);
+	struct path root;
+
+	/*
+	 * We have various synthetic filesystems that never get mounted.  On
+	 * these filesystems dentries are never used for lookup purposes, and
+	 * thus don't need to be hashed.  They also don't need a name until a
+	 * user wants to identify the object in /proc/pid/fd/.  The little hack
+	 * below allows us to generate a name for these objects on demand:
+	 *
+	 * Some pseudo inodes are mountable.  When they are mounted
+	 * path->dentry == path->mnt->mnt_root.  In that case don't call d_dname
+	 * and instead have d_path return the mounted path.
+	 */
+	if (path->dentry->d_op && path->dentry->d_op->d_dname &&
+	    (!IS_ROOT(path->dentry) || path->dentry != path->mnt->mnt_root))
+		return path->dentry->d_op->d_dname(path->dentry, buf, buflen);
+
+	rcu_read_lock();
+	get_fs_root_rcu(current->fs, &root);
+	if (unlikely(d_unlinked(path->dentry)))
+		prepend(&b, " (deleted)", 11);
+	else
+		prepend_char(&b, 0);
+	prepend_path(path, &root, &b);
+	rcu_read_unlock();
+
+	return extract_string(&b);
+}
+
+/** 
+ * include/linux/seq_buf.h
+ */
+
+/**
+ * seq_buf - seq buffer structure
+ * @buffer:	pointer to the buffer
+ * @size:	size of the buffer
+ * @len:	the amount of data inside the buffer
+ * @readpos:	The next position to read in the buffer.
+ */
+struct seq_buf {
+	char			*buffer;
+	size_t			size;
+	size_t			len;
+	loff_t			readpos;
+};
+
+/**
+ * seq_buf_get_buf - get buffer to write arbitrary data to
+ * @s: the seq_buf handle
+ * @bufp: the beginning of the buffer is stored here
+ *
+ * Return the number of bytes available in the buffer, or zero if
+ * there's no space.
+ */
+static inline size_t seq_buf_get_buf(struct seq_buf *s, char **bufp)
+{
+	return 0;
+}
+
+/**
+ * seq_buf_commit - commit data to the buffer
+ * @s: the seq_buf handle
+ * @num: the number of bytes to commit
+ *
+ * Commit @num bytes of data written to a buffer previously acquired
+ * by seq_buf_get.  To signal an error condition, or that the data
+ * didn't fit in the available space, pass a negative @num value.
+ */
+static inline void seq_buf_commit(struct seq_buf *s, int num)
+{
+}
+
+/** 
+ * lib/seq_buf.c
+ */
+
+char *mangle_path(char *s, const char *p, const char *esc);
+
+/**
+ * seq_buf_path - copy a path into the sequence buffer
+ * @s: seq_buf descriptor
+ * @path: path to write into the sequence buffer.
+ * @esc: set of characters to escape in the output
+ *
+ * Write a path name into the sequence buffer.
+ *
+ * Returns the number of written bytes on success, -1 on overflow
+ */
+int seq_buf_path(struct seq_buf *s, const struct path *path, const char *esc)
+{
+	char *buf;
+	size_t size = seq_buf_get_buf(s, &buf);
+	int res = -1;
+
+	WARN_ON(s->size == 0);
+
+	if (size) {
+		char *p = d_path(path, buf, size);
+		if (!IS_ERR(p)) {
+			char *end = mangle_path(buf, p, esc);
+			if (end)
+				res = end - buf;
+		}
+	}
+	seq_buf_commit(s, res);
+
+	return res;
+}
